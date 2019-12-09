e0a52373c99952845893af9480943f669142c345
brian <chickenfoot59@gmail.com>
2019-12-09 11:04:25 +0800

    fix naming problem and language id check function

--- http.py
+++ http.py
@@ -10,40 +10,41 @@ import glob
 app = Flask(__name__)
 @app.route('/submit',methods=['GET', 'POST'])
 def submit():
+    languages = ['c', 'cpp', 'py']
     if request.method == 'POST':
         file = request.files['upload_file']#get file
         file.save("C:/test/"+file.filename.split(".")[0]+'.zip')#save file
         
         checker=request.values['checker'] 
-        languageId=request.values['languageId'] #0:C,1:C++,2:python3
-        language_type=''#transfer languageID to language class
-        if languageId==0:
-            language_type="c"
-        elif languageId==1:
-            language_type="cpp"
+        language_id=request.values['languageId'] #0:C,1:C++,2:python3
+        language_type=''
+        if type(language_id)==int and 0<=language_id<=2:
+            language_type=languages[language_id]
         else:
-            language_type="py"
-            
+            return "language id wrong-400",400
+           
         token=request.values['token']
-        submissionId=request.values['submissionId']
+        submission_id=request.values['submissionId']
+        while type(submission_id)!=str:
+            return "submission id wrong-400",400
         
         archive = zipfile.ZipFile("C:/test/Submissions.zip", 'r')
         #file_name=archive.filename.split('.')[0]#filename
         archive.extractall('C:/test/')
         archive.close()
         
-        archize_src=zipfile.ZipFile('C:/test/'+submissionId+'/src.zip', 'r')
-        archize_src.extractall('C:/test/'+submissionId)
+        archize_src=zipfile.ZipFile('C:/test/'+submission_id+'/src.zip', 'r')
+        archize_src.extractall('C:/test/'+submission_id)
         archize_src.close()
         
-        archize_testcase=zipfile.ZipFile('C:/test/'+submissionId+'/testcase.zip', 'r')
-        archize_testcase.extractall('C:/test/'+submissionId)
+        archize_testcase=zipfile.ZipFile('C:/test/'+submission_id+'/testcase.zip', 'r')
+        archize_testcase.extractall('C:/test/'+submission_id)
         archize_testcase.close()
            
-        if len(os.listdir('C:/test/'+submissionId+'/src/'))==0:#len(['1','2','3'])
+        if len(os.listdir('C:/test/'+submission_id+'/src/'))==0:#len(['1','2','3'])
             return "under src does not have any file-400",400
         else:
-            for files in walk('C:/test/'+submissionId+'/src/'):
+            for files in walk('C:/test/'+submission_id+'/src/'):
                 print (files)
                 file_name=files[2][0].split(".")[0]#get file name
                 if file_name=="main":
@@ -59,9 +60,9 @@ def submit():
                         folder_out_1=glob.glob(target_path_out_1)
                         
                         if len(folder_in_0)==len(folder_out_0) and len(folder_in_1)==len(folder_out_1):
-                            if (os.path.isfile('C:/test/'+submissionId+'/meta.json')):
+                            if (os.path.isfile('C:/test/'+submission_id+'/meta.json')):
                                 # read file
-                                with open('C:/test/'+submissionId+'/meta.json', 'r') as myfile:
+                                with open('C:/test/'+submission_id+'/meta.json', 'r') as myfile:
                                     data=myfile.read()
                                 # parse file
                                 obj = json.loads(data)
