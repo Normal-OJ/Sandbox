9a3dacccaf029e3f5eaa5ae1f567cd5c5cb7b7e8
Siao, Yu-Jie <a6194315535@gmail.com>
2019-12-10 19:34:27 +0800

    add read_only mode

--- sandbox.py
+++ sandbox.py
@@ -4,7 +4,7 @@ import logging
 import docker
 
 
-class sandbox():
+class Sandbox():
     def __init__(self,
                  time_limit,
                  mem_limit,
@@ -14,29 +14,26 @@ class sandbox():
                  volume_readonly=True):
         with open('.config/submission.json') as f:
             config = json.load(f)
-        self.time_limit = time_limit # int:ms
-        self.mem_limit = mem_limit # int:kb
-        self.image = image # str
-        self.src_dir = src_dir # str
-        self.command = command # str
-        self.volume_readonly = volume_readonly # bool
+        self.time_limit = time_limit  # int:ms
+        self.mem_limit = mem_limit  # int:kb
+        self.image = image  # str
+        self.src_dir = src_dir  # str
+        self.command = command  # str
+        self.volume_readonly = volume_readonly  # bool
         self.client = docker.APIClient(base_url=config['docker_url'])
 
     def run(self):
-        if self.volume_readonly:
-            volume = {self.src_dir: {'bind': '/submission', 'mode': 'ro'}}
-        else:
-            volume = {self.src_dir: {'bind': '/submission', 'mode': 'rw'}}        
+        read_mode = 'ro' if self.volume_readonly else 'rw'
+        volume = {self.src_dir: {'bind': '/submission', 'mode': read_mode}}
         container_working_dir = '/submission'
         host_config = self.client.create_host_config(
             binds={self.src_dir: {
                 'bind': '/submission',
-                'mode': 'rw'
+                'mode': read_mode
             }})
         container = self.client.create_container(
             image=self.image,
             command=self.command,
-            detach=True,
             volumes=volume,
             network_disabled=True,
             working_dir=container_working_dir,
@@ -45,14 +42,20 @@ class sandbox():
             docker_msg = container.get('Warning')
             logging.warning(f'Warning: {docker_msg}')
 
-        self.client.start(container=container.get('Id'))
-        self.client.wait(container=container,
-                         timeout=5 * self.time_limit / 1000)
-        
-        stdout = self.client.logs(container=container,
-                                  stdout=True,
+        self.client.start(container)
+        exit_status = self.client.wait(container, timeout=5 * self.time_limit / 1000)
+
+        stdout = self.client.logs(container, stdout=True,
                                   stderr=False).decode('utf-8')
         stderr = self.client.logs(container, stdout=False,
                                   stderr=True).decode('utf-8')
         self.client.remove_container(container, v=True, force=True)
-        return  {'exit_code': 0, 'stdout': stdout, 'stderr': stderr, 'duration': 10, 'mem_usage': 200,'timeout': False} # exit_code:int stdout:str stderr:str duration:int(ms) mem_usage:int(kb) timeout:bool
+        return {
+            'Error': exit_status['Error'],
+            'ExitCode': exit_status['StatusCode'],
+            'Stdout': stdout,
+            'Stderr': stderr,
+            'Duration': 10,
+            'MemUsage': 200,
+            'Timeout': False
+        }  # Error:str Exit_code:int Stdout:str Stderr:str Duration:int(ms) MemUsage:int(kb) Timeout:bool
