6b28f50d7cc88297060cdb5ace8d814f28d0a6cb
Siao, Yu-Jie <a6194315535@gmail.com>
2019-12-24 16:15:57 +0800

    update docker environment

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@
 # sandbox binary
 rm -f sandbox
 wget https://github.com/Normal-OJ/C-Sandbox/releases/latest/download/sandbox
+chmod +x sandbox
 # c_cpp
 docker build -t c_cpp -f c_cpp_dockerfile . --no-cache
 # python3
 docker build -t python3 -f python3_dockerfile . --no-cache
+
 
