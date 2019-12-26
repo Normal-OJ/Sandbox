4946ef43939150ac2156f8000c18d6737135965b
Siao, Yu-Jie <a6194315535@gmail.com>
2019-12-26 13:06:18 +0800

    fix docker image naming(cannot use uppercase)

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ rm -f sandbox
 wget https://github.com/Normal-OJ/C-Sandbox/releases/latest/download/sandbox
 chmod +x sandbox
 # c_cpp
-docker build -t NOJ-C-CPP -f c_cpp_dockerfile . --no-cache
+docker build -t noj-c-cpp -f c_cpp_dockerfile . --no-cache
 # python3
-docker build -t NOJ-PY3 -f python3_dockerfile . --no-cache
+docker build -t noj-py3 -f python3_dockerfile . --no-cache
 
 
