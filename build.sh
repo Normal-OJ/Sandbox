d7ea334583b9e07654b599752caf49cb34ccf211
Siao, Yu-Jie <a6194315535@gmail.com>
2019-12-26 12:52:11 +0800

    update sandbox image naming

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ rm -f sandbox
 wget https://github.com/Normal-OJ/C-Sandbox/releases/latest/download/sandbox
 chmod +x sandbox
 # c_cpp
-docker build -t c_cpp -f c_cpp_dockerfile . --no-cache
+docker build -t NOJ-C-CPP -f c_cpp_dockerfile . --no-cache
 # python3
-docker build -t python3 -f python3_dockerfile . --no-cache
+docker build -t NOJ-PY3 -f python3_dockerfile . --no-cache
 
 
